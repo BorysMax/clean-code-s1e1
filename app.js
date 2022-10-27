03277260ec128c5859c0307ff6907827c60d8c02
Borys Maksymov <radiatorzp@gmail.com>
2022-10-27 19:37:27 +0300

    refactor: adjust HTML+CSS-formatting according to rule 1.2 in html-and-css.md

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ var deleteTask=function(){
 var taskCompleted=function(){
   console.log("Complete Task...");
 
-    //Append the task list item to the #completed-tasks
+  //Append the task list item to the #completed-tasks
   var listItem=this.parentNode;
   completedTasksHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskIncomplete);
